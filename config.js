050d9f320957a581dc901fb3e067fff1d10bb60b
anotherhabib <another.habib15@gmail.com>
2025-06-29 20:26:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,23 +5,22 @@ const config = {
     music: "assets/music/song.mp3",
     
     photos: [
-        { url: 'assets/images/foto1.jpg', description: 'First date', date: '15 Oct 2024' },
-        { url: 'assets/images/foto2.jpg', description: 'Banyuwangi Park', date: '28 Jan 2025' },
-        { url: 'assets/images/foto3.jpg', description: 'Foto Studio Jember', date: '29 Jan 2025' },
-        { url: 'assets/images/foto4.jpg', description: 'Sidang Skripsi', date: '06 May 2025' },
-        { url: 'assets/images/foto5.jpg', description: 'Jatim Park 3, Malang', date: '23 May 2025' },
-        { url: 'assets/images/foto6.jpg', description: 'Brakseng Batu, Malang', date: '24 May 2025' },
-        { url: 'assets/images/foto7.jpg', description: 'Kuliner Jember', date: '10 Jun 2025' },
-        { url: 'assets/images/foto8.jpg', description: 'Teluk Love, Jember', date: '11 Jun 2025' },
-        { url: 'assets/images/foto9.jpg', description: 'Teluk Love, Jember', date: '11 Jun 2025' }
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 Jan 2025' },
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 Feb 2025' },
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 Mar 2025' },
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 Apr 2025' },
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 May 2025' },
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 Jun 2025' },
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 Jul 2025' },
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 Aug 2025' },
+        { url: 'assets/images/foto10.jpg', description: 'Description', date: '15 Sep 2025' }
     ],
     
-    // Isi surat cinta
     loveLetter: {
-        title: "kenapa aku memilihmu?",
-        p1: "Aku memilihmu karena rasa nyaman yang kamu berikan belum pernah aku temui di manapun. Caramu memperlakukanku dengan begitu indah membuatku tak menginginkan siapapun lagi. Aku memilihmu dengan hati, bukan sekadar rasa suka atau penasaran.",
-        p2: "Denganmu, aku merasa cukup. Effortku dihargai, kehadiranku dianggap, dan perjuanganku tidak sia-sia. Kamu adalah bukti bahwa 'rumah' tidak selalu berbentuk bangunan, tapi bisa berupa seseorang.",
-        p3: "Aku mungkin tidak selalu sempurna, tapi aku pastikan kamu akan selalu jadi prioritasku. Tolong, jangan pernah pergi, karena tujuanku adalah menjadikanmu akhir dari perjalanan panjang ini, sekaligus teman hidupku selamanya.",
-        signature: "Dengan seluruh cintaku,<br>Habib"
+        title: "Judul Pesan",
+        p1: "Di bagian ini, Anda bisa menulis surat/pesan. Misalnya, menceritakan momen pertama kali bertemu atau alasan utama Anda memilihnya sebagai pasangan.",
+        p2: "Paragraf kedua bisa Anda gunakan untuk mengungkapkan betapa berartinya dia dalam hidup Anda saat ini. Ceritakan bagaimana kehadirannya membuat hari-hari Anda lebih baik.",
+        p3: "Gunakan paragraf terakhir ini untuk menuliskan harapan Anda untuk masa depan.",
+        signature: "Dengan seluruh cintaku,<br>[Nama Anda di Sini]"
     }
 };
